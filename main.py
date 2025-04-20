6139062f7849133ac542d0bc39c0705b27cb97d8
DatTriTat <tatdattri0306@gmail.com>
2025-04-20 13:18:29 -0700

    app: add entrypoint

---
+++ main.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+# Export the FastAPI app for ASGI servers.
+from app.main import app
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
